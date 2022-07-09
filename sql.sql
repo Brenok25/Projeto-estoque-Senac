f2dc73a2aba8913525e44c886f027a922c27161d
Brenok25 <87788919+Brenok25@users.noreply.github.com>
2022-07-09 16:39:04 -0300

    script sql - banco do estoque

--- sql.sql
+++ sql.sql
@@ -17,5 +17,36 @@ nome varchar(40),
 cnpj int,
 endereço varchar(40),
 primary key (cod)
-)
+);
 
+create table Compra_venda(
+cod int auto_increment,
+estoque int, # O q tem no estoque
+compra int, # Valor comprado
+venda int,
+final int, # Valor atualizado
+primary key (cod)
+);
+
+#create table Venda(
+#cod int auto_increment,
+#estoque int, # O q tem no estoque
+#venda int, # Valor vendido
+#final int, # Valor atualizado
+#primary key (cod)
+#);
+
+
+
+#insert into Compra (estoque,compra) value(100,100);
+select * From Compra_venda;
+select * From Produto;
+#select sum(comprado) from Compra where cod = 1;
+#update Compra set comprado = sum(cod + quant) where cod = 1;
+#select (estoque + compra) from Compra where cod = 2;
+#update  Compra set final = (select (estoque + compra) where cod = 2) where cod = 2;
+#update Produto set quantidade = (select final from Compra where cod = 2) where cod = 2;
+
+#insert into Compra_venda (estoque,venda) value(100,50);
+#update  Compra_venda set final = (select (estoque - venda) where cod = 1) where cod = 1;
+#update Produto set quantidade = (select final from Compra_venda where cod = 1) where cod = 1;
